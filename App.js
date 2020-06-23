06cd8c3cd90a3c21c6b7ba3d2a7809fb4f86bdfa
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-23 17:22:11 +0530

    todo item component and styles added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoitem';
 
 export default function App() {
   const [todos, setTodos] = useState([
--- components/todoitem.js
+++ components/todoitem.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function TodoItem({props}) {
+export default function TodoItem({ item }) {
 
+    return (
+     <TouchableOpacity>
+         <Text style={styles.item}>{item.text}</Text>
+     </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
-
+    item: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: "#bbb",
+        borderWidth: 1,
+        borderStyle: "dashed",
+        borderRadius: 10
+    }
 });
