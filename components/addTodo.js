f7ceed240a00dee028db9ff435e97be26ddd955d
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-23 17:43:49 +0530

    addTodo Component

---
+++ components/addTodo.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, TextInput, Button } from 'react-native';
+
+export default function AddTodo() {
+
+    const [text, setText] = useState('');
+
+    const changeHandler = (val) => {
+        setText(val);
+    }
+
+    return (
+        <View>
+            <TextInput
+                style={styles.input}
+                placeholder="new todo..."
+                onChangeText={changeHandler}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom:10,
+        paddingHorizontal: 8,
+        paddingVertical: 8,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ddd"
+    }
+});
