322cc776282a174263ac7888374fa4d30c643019
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-24 18:10:30 +0530

    alert added for web

--- App.js
+++ App.js
@@ -1,44 +1,52 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todoitem';
-import AddTodo from './components/addTodo';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  Platform,
+} from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoitem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1'},
-    { text: 'create an app', key: '2'},
-    { text: 'play on the switch', key: '3'},
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
   ]);
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key)
-    })
-  }
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
 
   const sumbitHandler = (text) => {
-    if(text.length > 3) {
-      setTodos((prevTodos)=>{
-        return [
-          { text: text, key: Math.random().toString()},
-          ...prevTodos
-        ]
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
       });
     } else {
-      Alert.alert("OOPS!", "Todos must be over 3 chars long", [
-        {text: "Understood", onPress: () => console.log('alert closed')}
-      ])
+      if (Platform.OS === "web") {
+        alert("OOPS! Todos must be over 3 chars long");
+      } else {
+        Alert.alert("OOPS!", "Todos must be over 3 chars long", [
+          { text: "Understood", onPress: () => console.log("alert closed") },
+        ]);
+      }
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Header/>
+      <Header />
       <View style={styles.content}>
-        <AddTodo sumbitHandler={sumbitHandler}/>
+        <AddTodo sumbitHandler={sumbitHandler} />
         <View style={styles.list}>
-          <FlatList 
+          <FlatList
             data={todos}
             renderItem={({ item }) => (
               <TodoItem item={item} pressHandler={pressHandler} />
@@ -53,12 +61,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   content: {
-    padding:40,
+    padding: 40,
   },
-  list:{
-    marginTop:20,
-  }
+  list: {
+    marginTop: 20,
+  },
 });
